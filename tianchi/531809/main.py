7e17e21de227fe1026f28f30d4a37215f264a827
Chuanjian Wang <me@ckeyer.com>
2020-07-16 21:52:47 +0800

    test

---
+++ tianchi/531809/main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+ 
+import numpy as np
+import sklearn
+import pandas as pd
+import os
+import sys
+import time
+import tensorflow as tf
+
+from tensorflow import keras
+
+print(tf.__version__)
+print(sys.version_info)
+for module in mpl, np, pd, sklearn, tf, keras:
+    print(module.__name__, module.__version__)
